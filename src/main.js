a10478197240884f65bee88f7c9ea66c3d52b919
Mohd Ovais <ovais.m@gmail.com>
2020-08-20 17:52:39 +0200

    local changes dump

--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
 const { minify } = Terser;
-const doc = document;
-const getElementById = doc.getElementById.bind(doc);
+const getElementById = id => document.getElementById(id);
 const sourceEl = getElementById("source");
 const targetEl = getElementById("minified");
 const sourceBytesEl = getElementById("source-size");
@@ -17,23 +16,12 @@ const cm = (textArea, isMinified) =>
     lineWrapping: isMinified,
   });
 
-const sourceCodeMirror = cm(sourceEl);
+const sourceCodeMirror = cm(sourceEl, false);
 const targetCodeMirror = cm(targetEl, true);
 
-/**
- * https://stackoverflow.com/questions/5515869/string-length-in-bytes-in-javascript/34332105#34332105#answer-23329386
- * @param {string} str
- */
 function byteLength(str) {
-  // returns the byte length of an utf8 string
-  var s = str.length;
-  for (var i = str.length - 1; i >= 0; i--) {
-    var code = str.charCodeAt(i);
-    if (code > 0x7f && code <= 0x7ff) s++;
-    else if (code > 0x7ff && code <= 0xffff) s += 2;
-    if (code >= 0xdc00 && code <= 0xdfff) i--; //trail surrogate
-  }
-  return s.toLocaleString() + " bytes";
+  const blob = new Blob([str]);
+  return blob.length.toLocaleString() + ' bytes';
 }
 
 /**
